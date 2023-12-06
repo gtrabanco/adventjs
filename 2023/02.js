436df54a84799be5fa6c12bae128fe760aef852f
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-12-06 19:50:38 +0100

    Fixed challenge 2 and the points got in challenge 2

--- 2023/02.js
+++ 2023/02.js
@@ -1,5 +1,6 @@
+// 270
 function manufacture(gifts, materials) {
-  const craftable = c => materials.indexOf(c);
+  const craftable = c => materials.includes(c);
   const isGiftCraftable = gift => Array.from(gift).every(craftable);
   return gifts.filter(isGiftCraftable);
 }
